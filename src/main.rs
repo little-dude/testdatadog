22cdb93e311a2f3e56f0ffd29ed8dfcc25eb886e
Corentin Henry <corentinhenry@gmail.com>
2021-03-26 07:36:54 +0100

    fix crash

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use tracing_subscriber::layer::SubscriberExt;
 
 #[tokio::main]
 async fn main() {
-    create_datadog_tracing_subscriber("http://localhost:8126");
+    let _uninstall = create_datadog_tracing_subscriber("http://localhost:8126");
     error!("error");
     warn!("warn");
     info!("info");
@@ -15,7 +15,7 @@ async fn main() {
     tokio::time::sleep(std::time::Duration::from_millis(1_000)).await;
 }
 
-fn create_datadog_tracing_subscriber(agent: &str) {
+fn create_datadog_tracing_subscriber(agent: &str) -> datadog::Uninstall {
     let (tracer, uninstall) = datadog::new_pipeline()
         .with_service_name("datadogtest")
         .with_version(datadog::ApiVersion::Version05)
@@ -23,13 +23,12 @@ fn create_datadog_tracing_subscriber(agent: &str) {
         .install()
         .unwrap();
 
-    // Never drop `uninstall` so that the pipeline is never removed
-    let _: &'static _ = Box::leak(Box::new(uninstall));
-
     // integration with `tracing`
     let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
     let subscriber = tracing_subscriber::Registry::default().with(telemetry);
 
     // Make the datadog subscriber the default one throughout the program
     tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    uninstall
 }
